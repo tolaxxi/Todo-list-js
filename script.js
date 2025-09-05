c7b211b942fbcad45c472c16466dca5ddbf1c0f3
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-09-05 05:08:41 +0100

    Chore: add error handling for quote function

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const quote_API = 'https://api.realinspire.live/v1/quotes/random?maxLength=100';
 getQuote();
 
 async function getQuote() {
-  const data = await fetch(quote_API);
-  const quote = await data.json();
-  quoteDisplay.textContent = `"${quote[0].content}" - ${quote[0].author}`;
+  try {
+    const data = await fetch(quote_API);
+    const quote = await data.json();
+    quoteDisplay.textContent = `"${quote[0].content}" - ${quote[0].author}`;
+  } catch (err) {
+    quoteDisplay.textContent = `There seem to be an issue please try again`;
+  }
 }
